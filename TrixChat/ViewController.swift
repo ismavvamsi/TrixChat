9e563e7544da44b73ebb643d84d252278f606ec9
Vamsikrishna, Tangudu <Tangudu.Vamsikrishna@emc.com>
2019-04-10 15:10:34 +0530

    Initial Commit

---
+++ TrixChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrixChat
+//
+//  Created by Vamsikrishna, Tangudu on 4/10/19.
+//  Copyright © 2019 Vamsikrishna, Tangudu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
